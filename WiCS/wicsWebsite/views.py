c4d0ab99fa23201a05bf54124fa551b360b0644c
Nichole Emmons <bemmons1@asu.edu>
2016-07-14 16:06:55 -0700

    Second iteration on gettin things set up

--- WiCS/wicsWebsite/views.py
+++ WiCS/wicsWebsite/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'homepage.html')
+
+
+def about_us(request):
+    return render(request, 'aboutUs.html')
+
+
+def documents(request):
+    return render(request, 'documents.html')
+
+
+def events(request):
+    return render(request, 'eventCalendar.html')
+
+
+def outreach(request):
+    return render(request, 'outreach.html')
+
+
+def image_archive(request):
+    return render(request, 'imageArchive.html')
